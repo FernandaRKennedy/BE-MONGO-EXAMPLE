0d630542a5b8616cac0db0bbc75b9d43eca47be3
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-05-07 13:14:24 -0400

    mongoose created

---
+++ controllers/user.js
@@ -0,0 +1,70 @@
+const router = require('express').Router()
+const User = require('../models/user')
+
+//Get retreive all user 
+router.get('/', async (req,res) =>{
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (error) {
+    console.log(error)
+    res.json({'message': 'error retrieving users'})
+  }
+})
+
+//Get retreive user by username 
+router.get('/username/:username', async (req,res) =>{
+    const { username } = req.params
+    try {
+      const users = await User.find({ username })
+      res.json(users)
+    } catch (error) {
+      console.log(error)
+      res.json({'message': 'error retrieving users'})
+    }
+  })
+
+  //Get retreive user byid
+router.get('/id/:id', async (req,res) =>{
+    const { id } = req.params
+    try {
+      const user = await User.findById(id)
+      res.json(user)
+    } catch (error) {
+      console.log(error)
+      res.json({'message': 'error retrieving users'})
+    }
+  })
+
+
+
+  //Get delet  user byid
+router.delete('/id/:id', async (req,res) =>{
+    const { id } = req.params
+    try {
+      const user = await User.findByIdAndDelete(id)
+      if(!user){
+        res.status(404).json({'message': 'user  doesn\'t exist'})
+      }else{
+        res.json({'message': 'error deleting user'})
+      }
+    } catch (error) {
+      console.log(error)
+      res.json({'message': 'error deleting user'})
+    }
+  })
+  
+//post creat user
+router.post('/', async (req, res) =>{
+   try {
+    // const{username, email, location, age} = req.body
+    const createdUser = await new User(req.body).save()
+    res.json(createdUser)
+   } catch (error) {
+    console.log(error)
+    res.json({'message': 'error creating user'})
+   }
+})
+
+
+module.exports = router
